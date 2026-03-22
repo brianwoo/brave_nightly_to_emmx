bc3f96d054dd00404409c9d7ab30388a9768bf80
Brian Woo <brian.is.coding@gmail.com>
2026-03-21 19:20:02 -0600

    added replace icons

---
+++ replace_res_icons.sh
@@ -0,0 +1,24 @@
+cp -f ./assets/res/mipmap-hdpi/app_icon ./Bravearm64Universal/res/mipmap-hdpi/app_icon
+cp -f ./assets/res/mipmap-hdpi/layered_app_icon ./Bravearm64Universal/res/mipmap-hdpi/layered_app_icon
+cp -f ./assets/res/mipmap-hdpi/layered_app_icon_background ./Bravearm64Universal/res/mipmap-hdpi/layered_app_icon_background
+cp -f ./assets/res/mipmap-hdpi/shortcut_icon_shadow ./Bravearm64Universal/res/mipmap-hdpi/shortcut_icon_shadow
+
+cp -f ./assets/res/mipmap-mdpi/app_icon ./Bravearm64Universal/res/mipmap-mdpi/app_icon
+cp -f ./assets/res/mipmap-mdpi/layered_app_icon ./Bravearm64Universal/res/mipmap-mdpi/layered_app_icon
+cp -f ./assets/res/mipmap-mdpi/layered_app_icon_background ./Bravearm64Universal/res/mipmap-mdpi/layered_app_icon_background
+cp -f ./assets/res/mipmap-mdpi/shortcut_icon_shadow ./Bravearm64Universal/res/mipmap-mdpi/shortcut_icon_shadow
+
+cp -f ./assets/res/mipmap-xhdpi/app_icon ./Bravearm64Universal/res/mipmap-xhdpi/app_icon
+cp -f ./assets/res/mipmap-xhdpi/layered_app_icon ./Bravearm64Universal/res/mipmap-xhdpi/layered_app_icon
+cp -f ./assets/res/mipmap-xhdpi/layered_app_icon_background ./Bravearm64Universal/res/mipmap-xhdpi/layered_app_icon_background
+cp -f ./assets/res/mipmap-xhdpi/shortcut_icon_shadow ./Bravearm64Universal/res/mipmap-xhdpi/shortcut_icon_shadow
+
+cp -f ./assets/res/mipmap-xxhdpi/app_icon ./Bravearm64Universal/res/mipmap-xxhdpi/app_icon
+cp -f ./assets/res/mipmap-xxhdpi/layered_app_icon ./Bravearm64Universal/res/mipmap
+cp -f ./assets/res/mipmap-xxhdpi/layered_app_icon_background ./Bravearm64Universal/res/mipmap-xxhdpi/layered_app_icon_background
+cp -f ./assets/res/mipmap-xxhdpi/shortcut_icon_shadow ./Bravearm64Universal/res/mipmap-xxhdpi/shortcut_icon_shadow
+
+cp -f ./assets/res/mipmap-xxxhdpi/app_icon ./Bravearm64Universal/res/mipmap-xxxhdpi/app_icon
+cp -f ./assets/res/mipmap-xxxhdpi/layered_app_icon ./Bravearm64Universal/res/mipmap-xxxhdpi/layered_app_icon
+cp -f ./assets/res/mipmap-xxxhdpi/layered_app_icon_background ./Bravearm64Universal/res/mipmap-xxxhdpi/layered_app_icon_background
+cp -f ./assets/res/mipmap-xxxhdpi/shortcut_icon_shadow ./Bravearm64Universal/res/mipmap-xxxhdpi/shortcut_icon_shadow
